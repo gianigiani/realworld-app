8a54894402ed5aac38d5612998fef0f285032887
GG <giani.goras@gmail.com>
2025-07-25 11:30:06 +0300

    Add register page

---
+++ src/pages/register/register.ts
@@ -0,0 +1,64 @@
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../features/auth/api/auth.service';
+import { AuthForm } from '../../features/auth/model/auth.interface';
+
+@Component({
+  selector: 'app-register',
+  imports: [ReactiveFormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.scss',
+})
+export class Register {
+  authForm: FormGroup<AuthForm>;
+  authService = inject(AuthService);
+  router = inject(Router);
+  errorMessage = signal('');
+
+  constructor() {
+    this.authForm = new FormGroup<AuthForm>({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+        nonNullable: true,
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+        nonNullable: true,
+      }),
+      username: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(3)],
+        nonNullable: true,
+      }),
+    });
+  }
+
+  onSubmit() {
+    this.authService
+      .register({
+        username: this.authForm.value.username!,
+        email: this.authForm.value.email!,
+        password: this.authForm.value.password!,
+      })
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          this.router.navigate(['/']);
+        },
+
+        error: (error) => {
+          if (error.error && error.error.message) {
+            this.errorMessage = error.error.message;
+          } else {
+            this.errorMessage.set('Registration failed. Please try again.');
+          }
+          console.error('Registration failed:', error);
+        },
+      });
+  }
+}
