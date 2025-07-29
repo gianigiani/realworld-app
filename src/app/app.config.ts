0d29700198a0076857f2bbaaa6b0b3c5f04c0448
GG <giani.goras@gmail.com>
2025-07-29 17:12:12 +0300

    Add token interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,39 @@
 import {
   ApplicationConfig,
+  inject,
+  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { EMPTY } from 'rxjs';
 import { baseUrlInterceptor } from '../features/auth/api/api.interceptor';
+import { AuthService } from '../features/auth/api/auth.service';
+import { tokenInterceptor } from '../features/auth/api/token.interceptor';
 import { routes } from './app.routes';
+
+export function initAuth(authService: AuthService) {
+  return () => (authService.getToken() ? authService.getCurrentUser() : EMPTY);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([baseUrlInterceptor])),
+    provideHttpClient(
+      withInterceptors([baseUrlInterceptor, tokenInterceptor]),
+      withFetch(),
+    ),
+    provideAppInitializer(() => {
+      const initializerFn = initAuth(inject(AuthService));
+      return initializerFn();
+    }),
   ],
 };
