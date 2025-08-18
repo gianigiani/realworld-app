ec4647094add0227f1ddbb156e4e258a0f865434
GG <giani.goras@gmail.com>
2025-08-18 12:12:57 +0300

    Create article form

--- src/pages/editor/editor.ts
+++ src/pages/editor/editor.ts
@@ -1,9 +1,77 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { ArticleService } from '../../features/article/service/article.service';
+import { AuthService } from '../../features/auth/service/auth.service';
+import { ArticleForm } from '../../features/editor/model/articleForm.interface';
 
 @Component({
   selector: 'app-editor',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './editor.html',
   styleUrl: './editor.scss',
 })
-export class Editor {}
+export class Editor {
+  private articleService = inject(ArticleService);
+  private router = inject(Router);
+  authService = inject(AuthService);
+
+  articleForm: FormGroup<ArticleForm> = new FormGroup<ArticleForm>({
+    title: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+    description: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+    body: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+  });
+
+  tagField = new FormControl('');
+  tagList = signal<string[]>([]);
+
+  addTag() {
+    const tag = this.tagField.value;
+    if (tag != null && tag.trim() !== '' && this.tagList().indexOf(tag) < 0) {
+      this.tagList.update((currentTags) => [...currentTags, tag.trim()]);
+    }
+
+    this.tagField.reset();
+  }
+
+  removeTag(tagToRemove: string) {
+    this.tagList.update((currentTags) =>
+      currentTags.filter((tag) => tag !== tagToRemove),
+    );
+  }
+
+  onSubmit() {
+    this.addTag();
+
+    if (this.articleForm.valid) {
+      const formData = {
+        ...this.articleForm.value,
+        tagList: this.tagList(),
+      };
+
+      this.articleService.createArticle(formData).subscribe({
+        next: (article) => {
+          this.router.navigate(['/article/', article.slug]);
+        },
+        error: (error) => {
+          this.authService.errorMessage.set(error.error.message); //TODO: check msg error
+        },
+      });
+    }
+  }
+}
