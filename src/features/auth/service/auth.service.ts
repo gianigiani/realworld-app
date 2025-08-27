91eb7521c401edb085b4cfa4cb76843da5b6fb1c
GG <giani.goras@gmail.com>
2025-08-27 10:05:48 +0300

    Remove getToken function

--- src/features/auth/service/auth.service.ts
+++ src/features/auth/service/auth.service.ts
@@ -116,10 +116,6 @@ export class AuthService {
     );
   }
 
-  getToken(): string | null {
-    return this.tokenService.getValidToken();
-  }
-
   logout() {
     this.toastService.showSuccess('See you soon!');
     this.tokenService.remove();
